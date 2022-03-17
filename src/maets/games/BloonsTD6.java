ee222e5356ca769464c694dc2c862347ad4b5716
Keu <76072942+Keuxd@users.noreply.github.com>
2022-03-17 03:59:35 -0300

    Base save implementation for btd6
    download() and install() will be implemented later

---
+++ src/maets/games/BloonsTD6.java
@@ -0,0 +1,18 @@
+package maets.games;
+
+public class BloonsTD6 extends AbstractGame {
+	
+	public BloonsTD6() {
+		super(System.getenv("APPDATA") + "\\Goldberg SteamEmu Saves\\960090","960090","Maets/BloonsTD6");
+	}
+
+	@Override
+	public void download() {
+
+	}
+
+	@Override
+	public void install() {
+
+	}
+}
