eaad96a1482ac5dddbff8c81cb1303ff02ec70d6
Keu <76072942+Keuxd@users.noreply.github.com>
2022-03-17 03:58:38 -0300

    Bio4 class renamed to Bio4UHD
    Since we'll support the first game version too, the UHD makes more sense.
    
    The default path used is from the hd project one, not sure if we'll support the hd project or the UHD base, but they have the same save file, so remote path will be Bio4UHD for both

---
+++ src/maets/games/Bio4UHD.java
@@ -0,0 +1,18 @@
+package maets.games;
+
+public class Bio4UHD extends AbstractGame {
+	
+	public Bio4UHD() {
+		super("C:\\Games\\Resident Evil 4 - HD Project\\Bin32\\Profile\\Player\\Saves","Saves","Maets/Bio4UHD");
+	}
+	
+	@Override
+	public void download() {
+		System.out.println("BIO4 DOWNLOAD");
+	}
+
+	@Override
+	public void install() {
+		
+	}
+}
