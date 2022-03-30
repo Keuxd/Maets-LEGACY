4900a76babb7395186c762a103e6a5e4f8693772
Keu <76072942+Keuxd@users.noreply.github.com>
2022-03-30 09:48:50 -0300

    initFileSizeLoggingThread() added
    Since download buffer will be a "feature" this class will have both ways of download, using native calls and buffers in Files.copy() and manual ones using byte[]
    
    initFileSizeLogginThread(): since Files.copy method doesn't allow the program to go on until it finishes with the inputstream, we'll have a thread that will take a look at this file each second in order to update our SSM and logs.

--- src/maets/download/CloudStorageDownloader.java
+++ src/maets/download/CloudStorageDownloader.java
@@ -5,6 +5,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.concurrent.TimeUnit;
 
 public abstract class CloudStorageDownloader {
 	
@@ -12,20 +17,54 @@ public abstract class CloudStorageDownloader {
 	
 	protected static void downloadFromUrl(String link, String localFilePath) throws IOException {
 		URL url = new URL(link);
-		SizeScaleManager ssm = new SizeScaleManager(url.openConnection().getContentLengthLong());
+		long totalFileSize = url.openConnection().getContentLengthLong();
+		
+		Path localPath = Paths.get(localFilePath);
+		Thread logThread = initFileSizeLoggingThread(localPath, totalFileSize);
 
+		try (InputStream in = url.openStream()){
+			logThread.start();
+			Files.copy(in, localPath, StandardCopyOption.REPLACE_EXISTING);
+		} catch(Exception e) {
+			System.out.println("Download failed, logging thread will be interrupted");
+			logThread.interrupt();
+		}
+	}
+	
+	// Most cases buffer size will be a multiple of 1024, it's standard value is 8x == 8192
+	protected static void downloadFromUrl(String link, String localFilePath, int bufferSize) throws IOException {
+		if(bufferSize <= 512) bufferSize = 512;
+		URL url = new URL(link);
+		SizeScaleManager ssm = new SizeScaleManager(url.openConnection().getContentLengthLong());
+		
 		try (InputStream in = url.openStream();
-			BufferedInputStream bis = new BufferedInputStream(in);
-	         
-			FileOutputStream fos = new FileOutputStream(localFilePath)) {
-			 	byte[] data = new byte[1024];
-			 	int count;
-	            
-			 	while ((count = bis.read(data, 0, 1024)) != -1) {
-			 		fos.write(data, 0, count);
-			 		ssm.add(count);
-			 		System.out.println(ssm.getSize());
-			 	}
-	        }
+				BufferedInputStream bis = new BufferedInputStream(in);
+		         
+				FileOutputStream fos = new FileOutputStream(localFilePath)) {
+				 	byte[] data = new byte[bufferSize];
+				 	int count;
+				 	
+				 	while ((count = bis.read(data, 0, bufferSize)) != -1) {
+				 		fos.write(data, 0, count);
+				 		ssm.add(count);
+				 		System.out.println(ssm);
+				 	}
+				}
+	}
+	
+	private static Thread initFileSizeLoggingThread(Path localFilePath, long fileSize) {
+		return new Thread(() -> {
+			try {
+				SizeScaleManager ssm = new SizeScaleManager(fileSize);
+
+				while(!ssm.isFinished()) {
+					TimeUnit.SECONDS.sleep(1);
+					ssm.setBytes(Files.size(localFilePath));
+					System.out.println(ssm);
+				}
+			} catch(InterruptedException | IOException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 }
