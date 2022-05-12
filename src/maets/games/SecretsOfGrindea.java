f1d9062bb2b26662db41a56a8c565c77bab360be
Keu <76072942+Keuxd@users.noreply.github.com>
2022-05-12 04:20:37 -0300

    there will be support for SoG

---
+++ src/maets/games/SecretsOfGrindea.java
@@ -0,0 +1,29 @@
+package maets.games;
+
+public class SecretsOfGrindea extends AbstractGame {
+
+	private final String DEFAULT_PATH = "C:\\Maets\\Secrets of Grindea"; 
+	
+	public SecretsOfGrindea() {
+		super(System.getenv("APPDATA") + "\\Secrets of Grindea", "Secrets of Grindea", "Maets/Secrets of Grindea");
+	}
+
+	@Override
+	public void download() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void install() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void uninstall() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
