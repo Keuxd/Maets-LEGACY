549979454a7e0e9c3ccaab2092df5e3daec11153
Keu <76072942+Keuxd@users.noreply.github.com>
2022-05-12 04:17:46 -0300

    trying other arguments in url
    i'll probably have to use drive api

--- src/maets/download/Drive.java
+++ src/maets/download/Drive.java
@@ -8,7 +8,9 @@ public class Drive extends CloudStorageDownloader {
 	private Drive() {}
 
 	public static void downloadFile(String fileId, String localFilePath) throws IOException {
-		downloadFromUrl("https://drive.google.com/uc?export=download&id=" + fileId + "&confirm=t", localFilePath);
+//		downloadFromUrl("https://drive.google.com/uc?export=download&id=" + fileId + "&confirm=t", localFilePath);
+		downloadFromUrl("https://drive.google.com/uc?export=download&confirm=CfLW&id=" + fileId, localFilePath);
+//		downloadFromUrl("https://drive.google.com/file/d/" + fileId + "/view", localFilePath);
 	}
 
 	public static boolean isFileAvailable(String fileId) throws IOException {
