26aaa592e8ab690cc864178cb56b44a26ef418f2
Keu <76072942+Keuxd@users.noreply.github.com>
2022-05-12 04:21:10 -0300

    updated btd6 model

--- src/maets/games/BloonsTD6.java
+++ src/maets/games/BloonsTD6.java
@@ -1,17 +1,19 @@
 package maets.games;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 import com.github.junrar.Junrar;
 import com.github.junrar.exception.RarException;
 
+import maets.core.ExtractUtil;
 import maets.mega.Mega;
 import maets.mega.exceptions.MegaException;
 
 public class BloonsTD6 extends AbstractGame {
 	
-	private final String DOWNLOAD_FILE_NAME = "Bloons.TD.6.v30.2.5032.rar";
+	private final String DEFAULT_PATH = "C:\\Maets\\BloonsTD6";
 	
 	public BloonsTD6() {
 		super(System.getenv("APPDATA") + "\\Goldberg SteamEmu Saves\\960090","960090","Maets/BloonsTD6");
@@ -19,35 +21,50 @@ public class BloonsTD6 extends AbstractGame {
 
 	@Override
 	public void download() throws MegaException {
-		String defaultPath = "C:\\Maets\\BloonsTD6";
-		File dir = new File(defaultPath);
+		File dir = new File(DEFAULT_PATH);
 		dir.mkdirs();
 		
-		Mega.download("https://mega.nz/file/CfplVaAK#s8yu0h_AujWBW6teC8llsPOx7abFhPnHihSqDMwkYfI", defaultPath);
+		Mega.download("https://mega.nz/file/CfplVaAK#s8yu0h_AujWBW6teC8llsPOx7abFhPnHihSqDMwkYfI", DEFAULT_PATH);
 	}
 
 	@Override
-	public void install() {
-		try {
-			String defaultDownloadFilePath = "C:\\Maets\\BloonsTD6\\" + DOWNLOAD_FILE_NAME;
-			
-			System.out.println("Starting extraction...");
-			Junrar.extract(defaultDownloadFilePath, "C:\\Maets\\BloonsTD6");
-			System.out.println("Finishing extraction...");
-			
-			if(new File(defaultDownloadFilePath).delete()) {
-				System.out.println("Rar file deleted successfully");
-			} else {
-				System.out.println("ERROR - Rar file not deleted");
+	public void install() throws IOException, RarException {
+		File rar = new File(DEFAULT_PATH).listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".rar");
 			}
-		} catch (IOException | RarException e) {
-			e.printStackTrace();
+		})[0];
+		
+		System.out.println("Starting extraction...");
+		ExtractUtil.extract(rar.getPath(), DEFAULT_PATH);
+		System.out.println("Finishing extraction...");
+		
+		if(rar.delete()) {
+			System.out.println("Rar file deleted successfully");
+		} else {
+			System.out.println("ERROR - Rar file not deleted");
+		}
+	}
+	
+	@Override
+	public void uninstall() {
+		if(deleteDir(new File(DEFAULT_PATH))) {
+			System.out.println("BloonsTD6 uninstalled successfully");
+		} else {
+			System.out.println("Error uninstalling BloonsTD6");
 		}
 	}
 	
 	public void run() throws IOException {
-		String downloadFileNameWithoutExtension = DOWNLOAD_FILE_NAME.substring(0, DOWNLOAD_FILE_NAME.length()-4);
-		String defaultInstalationPath = "C:\\Maets\\BloonsTD6\\" + downloadFileNameWithoutExtension + "\\" + downloadFileNameWithoutExtension + "\\";
-		Runtime.getRuntime().exec("C:\\Maets\\BloonsTD6\\Bloons.TD.6.v30.2.5032\\Bloons.TD.6.v30.2.5032\\BloonsTD6.exe", null, new File(defaultInstalationPath));
+		String firstGameFolderName = new File(DEFAULT_PATH).list(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return dir.isDirectory();
+			}
+		})[0];
+		
+		File secondGameFolder = new File(DEFAULT_PATH + "\\" + firstGameFolderName + "\\" + firstGameFolderName);
+		Runtime.getRuntime().exec(secondGameFolder + "\\BloonsTD6.exe", null, secondGameFolder);
 	}
 }
