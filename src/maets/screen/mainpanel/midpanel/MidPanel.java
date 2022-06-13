da034f9d8fa7308087d05e87f60c32d9c9590d1a
Keu <76072942+Keuxd@users.noreply.github.com>
2022-06-12 21:57:01 -0300

    added gradientlines

--- src/maets/screen/mainpanel/midpanel/MidPanel.java
+++ src/maets/screen/mainpanel/midpanel/MidPanel.java
@@ -1,16 +1,18 @@
 package maets.screen.mainpanel.midpanel;
 
-import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import maets.core.Main;
 import maets.screen.mainpanel.ButtonsResponsivityMouseAdapter;
-import maets.screen.mainpanel.upperpanel.UserPanel;
 
 public class MidPanel extends JPanel {
 
@@ -76,5 +78,33 @@ public class MidPanel extends JPanel {
 			}
 		});
 		add(btnNewButton_3);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+        Graphics2D g2 = (Graphics2D) g;
+
+        Color c1 = new Color(0x4E5052); // Lighter LAF background color
+        Color c2 = new Color(0x3C3F41); // Darker LAF background color
+        
+        final int LINE_HEIGHT = 3;
+        
+        GradientPaint gradientUpLeft = new GradientPaint(0, 0, c2, getWidth() / 2, LINE_HEIGHT, c1);
+        g2.setPaint(gradientUpLeft);
+        g2.fill(new Rectangle2D.Double(0, 0, getWidth() / 2, LINE_HEIGHT));
+        
+        GradientPaint gradientUpRight = new GradientPaint(getWidth() / 2, 0, c1, getWidth(), LINE_HEIGHT, c2);
+        g2.setPaint(gradientUpRight);
+        g2.fill(new Rectangle2D.Double(getWidth() / 2, 0, getWidth() / 2, LINE_HEIGHT));
+        
+        GradientPaint gradientDownLeft = new GradientPaint(0, getHeight() - LINE_HEIGHT, c2, getWidth() / 2, getHeight(), c1);
+        g2.setPaint(gradientDownLeft);
+        g2.fill(new Rectangle2D.Double(0, getHeight() - LINE_HEIGHT, getWidth() / 2, LINE_HEIGHT));
+        
+        GradientPaint gradientDownRight = new GradientPaint(getWidth() / 2, getHeight() - LINE_HEIGHT, c1, getWidth(), getHeight(), c2);
+        g2.setPaint(gradientDownRight);
+        g2.fill(new Rectangle2D.Double(getWidth() / 2, getHeight() - LINE_HEIGHT, getWidth() / 2, LINE_HEIGHT));
 	}
 }
