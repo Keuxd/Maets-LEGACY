3a6d257044828eb5d64c08218e07e18f8e5edf88
Keu <76072942+Keuxd@users.noreply.github.com>
2022-06-12 21:55:42 -0300

    removed paint test
    i just realize it would be better if i put those lines in MidPanel and make it bigger

--- src/maets/screen/mainpanel/MainPanel.java
+++ src/maets/screen/mainpanel/MainPanel.java
@@ -1,16 +1,13 @@
 package maets.screen.mainpanel;
 
-import java.awt.Color;
-import java.awt.GradientPaint;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import maets.core.Main;
+import maets.mega.exceptions.MegaException;
 import maets.screen.mainpanel.bottompanel.BottomPanel;
 import maets.screen.mainpanel.midpanel.MidPanel;
+import maets.screen.mainpanel.upperpanel.UpperPanel;
 
 public class MainPanel extends JPanel {
 
@@ -22,33 +19,18 @@ public class MainPanel extends JPanel {
 		setBounds(0, 0, parentFrame.getWidth(), parentFrame.getHeight());
 		setLayout(null);
 		
-//		JPanel upperPanel = null;
-//		try {
-//			upperPanel = new UpperPanel(parentFrame);
-//		} catch(MegaException e1) {
-//			Main.unexpectedError(e1.getMessage(), parentFrame);
-//		}
-//		add(upperPanel);
+		JPanel upperPanel = null;
+		try {
+			upperPanel = new UpperPanel(parentFrame);
+		} catch(MegaException e1) {
+			Main.unexpectedError(e1.getMessage(), parentFrame);
+		}
+		add(upperPanel);
 		
 		MidPanel mp = new MidPanel();
 		add(mp);
 		
 		BottomPanel bp = new BottomPanel();
 		add(bp);
-		
-		JPanel p = new JPanel() {
-			@Override
-			public void paintComponent(Graphics g) {
-				super.paintComponent(g);
-		        Graphics2D g2 = (Graphics2D) g;
-
-		        GradientPaint btg = new GradientPaint(0,150,Color.red, 1920, 100, Color.blue);
-		        g2.setPaint(btg);
-		        g2.fill(new Rectangle2D.Double(0, 150, 1920, 100));
-			}
-		};
-		p.setBounds(0,0,1920,200);
-		
-		add(p);
 	}
 }
