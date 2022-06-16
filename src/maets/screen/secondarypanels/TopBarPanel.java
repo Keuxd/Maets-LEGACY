588e068d6a1a9bed0160deb54b889d97ec9132a6
Keu <76072942+Keuxd@users.noreply.github.com>
2022-06-16 00:00:15 -0300

    Create TopBarPanel.java
    this class will be the top bar with the return button, we might have timer too but not 100% yet

---
+++ src/maets/screen/secondarypanels/TopBarPanel.java
@@ -0,0 +1,19 @@
+package maets.screen.secondarypanels;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class TopBarPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public TopBarPanel() {
+
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		
+	}
+}
