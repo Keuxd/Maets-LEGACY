d2721252122d064eb01da805c0a4216034f7478d
Keu <76072942+Keuxd@users.noreply.github.com>
2022-06-21 02:49:31 -0300

    added bounds line

--- src/maets/screen/secondarypanels/TopBarPanel.java
+++ src/maets/screen/secondarypanels/TopBarPanel.java
@@ -1,6 +1,10 @@
 package maets.screen.secondarypanels;
 
+import java.awt.Color;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JPanel;
 
@@ -14,6 +18,21 @@ public class TopBarPanel extends JPanel {
 	
 	@Override
 	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		
+        Graphics2D g2 = (Graphics2D) g;
+
+        Color c1 = new Color(0x4E5052); // Lighter LAF background color
+        Color c2 = new Color(0x3C3F41); // Darker LAF background color
+        
+        final int LINE_HEIGHT = 3;
+        
+        GradientPaint gradientDownLeft = new GradientPaint(0, getHeight() - LINE_HEIGHT, c2, getWidth() / 2, getHeight(), c1);
+        g2.setPaint(gradientDownLeft);
+        g2.fill(new Rectangle2D.Double(0, getHeight() - LINE_HEIGHT, getWidth() / 2, LINE_HEIGHT));
+        
+        GradientPaint gradientDownRight = new GradientPaint(getWidth() / 2, getHeight() - LINE_HEIGHT, c1, getWidth(), getHeight(), c2);
+        g2.setPaint(gradientDownRight);
+        g2.fill(new Rectangle2D.Double(getWidth() / 2, getHeight() - LINE_HEIGHT, getWidth() / 2, LINE_HEIGHT));
 	}
 }
