d0c26a9b291b3ad4775b622879d229c2775d2d1a
Keu <76072942+Keuxd@users.noreply.github.com>
2022-06-21 02:47:23 -0300

    Create GamesPanel.java

---
+++ src/maets/screen/secondarypanels/storepanel/GamesPanel.java
@@ -0,0 +1,13 @@
+package maets.screen.secondarypanels.storepanel;
+
+import javax.swing.JPanel;
+
+public class GamesPanel extends JPanel {
+
+	private static final long serialVersionUID = -8271674911298891309L;
+
+	public GamesPanel() {
+		setBounds(0, 110, 1920, 1080);
+		setLayout(null);
+	}
+}
