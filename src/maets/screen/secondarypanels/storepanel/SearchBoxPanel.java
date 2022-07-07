78065da82a39ee79cda11e0de8021cd6a54e6b4f
Keu <keuxd2003@gmail.com>
2022-07-07 01:43:34 -0300

    setting CellRendererComponent text to null
    since we just need it's string for checks it can't be rendered

--- src/maets/screen/secondarypanels/storepanel/SearchBoxPanel.java
+++ src/maets/screen/secondarypanels/storepanel/SearchBoxPanel.java
@@ -54,6 +54,8 @@ public class SearchBoxPanel extends JPanel {
 			public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
 		        Icon icon = gt.getIcon((String) value);
 		    	JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, false, cellHasFocus);
+		    	label.setText(null);
+
 		        label.setIcon(icon);
 		        return label;
 		    }
