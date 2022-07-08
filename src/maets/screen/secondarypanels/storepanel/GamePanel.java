159e20276382dab02be328c7e0d1421d37b83f86
Keu <keuxd2003@gmail.com>
2022-07-08 01:54:24 -0300

    bounds adjust

--- src/maets/screen/secondarypanels/storepanel/GamePanel.java
+++ src/maets/screen/secondarypanels/storepanel/GamePanel.java
@@ -7,7 +7,7 @@ public class GamePanel extends JPanel {
 	private static final long serialVersionUID = -8271674911298891309L;
 
 	public GamePanel() {
-		setBounds(410, 110, 1920, 1080);
+		setBounds(490, 110, 1510, 970);
 		setLayout(null);
 	}
 }
