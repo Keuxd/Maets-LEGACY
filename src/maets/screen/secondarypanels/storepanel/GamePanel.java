4fbdd04c590c10c1276dc8db83198307980103e6
Keu <keuxd2003@gmail.com>
2022-07-08 02:01:14 -0300

    GamePanel implemented
    for now we only have a JButton for testing, but here we'll have the default screen for download games..
    
    we might have all games "download pages" here being populated in methods, which is not perfect but might give some flexibility

--- src/maets/screen/secondarypanels/storepanel/GamePanel.java
+++ src/maets/screen/secondarypanels/storepanel/GamePanel.java
@@ -1,13 +1,35 @@
 package maets.screen.secondarypanels.storepanel;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
 
 	private static final long serialVersionUID = -8271674911298891309L;
 
+	private String gameName;
+	
 	public GamePanel() {
+		this.gameName = "";
 		setBounds(490, 110, 1510, 970);
 		setLayout(null);
+	}
+	
+	protected void populatePanel(String gameName) {
+		this.gameName = gameName;
+		
+		JButton btnNewButton = new JButton(gameName);
+		btnNewButton.setBounds(0, 852, 228, 169);
+		add(btnNewButton);
+	}
+	
+	protected void repopulatePanel(String gameName) {
+		removeAll();
+		repaint();
+		populatePanel(gameName);
+	}
+	
+	protected String getGameName() {
+		return this.gameName;
 	}
 }
