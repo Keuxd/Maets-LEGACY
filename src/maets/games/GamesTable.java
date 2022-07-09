e25f4e58ddba90a39c4edb703479c337b4ab3401
Keu <keuxd2003@gmail.com>
2022-07-09 02:26:00 -0300

    using multiple hashmaps,registerGame() implemented
    In this new model we gotta keep banner, samples and description in cache in order to keep the jpanel switch smooth and we'll use multiple hashmaps to storage everything

--- src/maets/games/GamesTable.java
+++ src/maets/games/GamesTable.java
@@ -14,23 +14,49 @@ import maets.core.Resources.ResourceType;
 public class GamesTable {
 	
 	private Map<String, ImageIcon> games;
+	private Map<String, ImageIcon[]> samples;
+	private Map<String, String[]> descriptions;
 	
-	public GamesTable(int bannerWidth, int bannerHeight) {
+	public GamesTable(int bannerWidth, int bannerHeight, int sampleWidth, int sampleHeight) {
 		games = new ConcurrentHashMap<>();
-		games.put("Vampire Survivors", Resources.getImageIconResized("vampire-survivors-banner.jpg", ResourceType.GAMES, bannerWidth, bannerHeight, Image.SCALE_AREA_AVERAGING));
-		games.put("Resident Evil 4 UHD", Resources.getImageIconResized("resident-evil-4-uhd-banner.jpg", ResourceType.GAMES, bannerWidth, bannerHeight, Image.SCALE_AREA_AVERAGING));
-		games.put("Digimon Story Cyber Sleuth: Complete Edition", Resources.getImageIconResized("digimon-cyber-sleuth-banner.jpg", ResourceType.GAMES, bannerWidth, bannerHeight, Image.SCALE_AREA_AVERAGING));
+		samples = new ConcurrentHashMap<>();
+		
+		registerGame("Vampire Survivors",
+				Resources.getImageIconResized("vampire-survivors-banner.jpg", ResourceType.GAMES, bannerWidth, bannerHeight, Image.SCALE_AREA_AVERAGING),
+				Resources.getImageIconResized("vampire-survivors-sample-1.jpg", ResourceType.GAMES, sampleWidth, sampleHeight, Image.SCALE_AREA_AVERAGING));
+		
+		registerGame("Resident Evil 4 UHD",
+				Resources.getImageIconResized("resident-evil-4-uhd-banner.jpg", ResourceType.GAMES, bannerWidth, bannerHeight, Image.SCALE_AREA_AVERAGING),
+				Resources.getImageIconResized("resident-evil-4-uhd-sample-1.jpg", ResourceType.GAMES, sampleWidth, sampleHeight, Image.SCALE_AREA_AVERAGING));
+		
+		registerGame("Digimon Story Cyber Sleuth: Complete Edition",
+				Resources.getImageIconResized("digimon-cyber-sleuth-banner.jpg", ResourceType.GAMES, bannerWidth, bannerHeight, Image.SCALE_AREA_AVERAGING),
+				Resources.getImageIconResized("digimon-cyber-sleuth-sample-1.jpg", ResourceType.GAMES, sampleWidth, sampleHeight, Image.SCALE_AREA_AVERAGING));
+
+	}
+	
+	private void registerGame(String name, ImageIcon banner, ImageIcon... samples) {
+		this.games.put(name, banner);
+		this.samples.put(name, samples);
 	}
 	
 	public Set<String> getNames() {
 		return games.keySet();
 	}
 	
-	public Collection<ImageIcon> getIcons() {
+	public Collection<ImageIcon> getBanners() {
 		return games.values();
 	}
 	
-	public ImageIcon getIcon(String iconName) {
-		return games.get(iconName);
+	public Collection<ImageIcon[]> getSamples() {
+		return samples.values();
+	}
+	
+	public ImageIcon getBanner(String gameName) {
+		return games.get(gameName);
+	}
+	
+	public ImageIcon[] getSamples(String gameName) {
+		return samples.get(gameName);
 	}
 }
