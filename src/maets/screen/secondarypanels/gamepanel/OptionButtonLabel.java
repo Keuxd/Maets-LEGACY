bfea0be874a4301f19b16446741c908f88363e59
Keu <keuxd2003@gmail.com>
2022-10-12 21:13:06 -0300

    highlight label for optionbuttonlabel

--- src/maets/screen/secondarypanels/gamepanel/OptionButtonLabel.java
+++ src/maets/screen/secondarypanels/gamepanel/OptionButtonLabel.java
@@ -58,6 +58,26 @@ public class OptionButtonLabel extends JLabel {
 	}
 	
 	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		if(isClickValid()) {
+	        Graphics2D g2 = (Graphics2D) g;
+
+	        Color c1 = new Color(0x4E5052); // Lighter LAF background color
+	        Color c2 = new Color(0x3C3F41); // Darker LAF background color
+	        c1 = Color.LIGHT_GRAY;
+	        
+	        final int LINE_HEIGHT = 3;
+	        
+	        GradientPaint gradientDownLeft = new GradientPaint(0, getHeight() - LINE_HEIGHT, c2, getWidth() / 2, getHeight(), c1);
+	        g2.setPaint(gradientDownLeft);
+	        g2.fill(new Rectangle2D.Double(0, getHeight() - LINE_HEIGHT, getWidth() / 2, LINE_HEIGHT));
+	        
+	        GradientPaint gradientDownRight = new GradientPaint(getWidth() / 2, getHeight() - LINE_HEIGHT, c1, getWidth(), getHeight(), c2);
+	        g2.setPaint(gradientDownRight);
+	        g2.fill(new Rectangle2D.Double(getWidth() / 2, getHeight() - LINE_HEIGHT, getWidth() / 2, LINE_HEIGHT));
+		}
 	
 	}
 }
